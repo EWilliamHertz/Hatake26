64c0478dd165198913868d9c7d90c4382f81451e
ewilliamhertz <ewilliamhe@gmail.com>
2026-04-07 13:43:27 +0000

    added locale to request.ts

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,8 +1,16 @@
 import { getRequestConfig } from 'next-intl/server';
+import { notFound } from 'next/navigation';
+
+const locales = ['en', 'sv'];
 
 export default getRequestConfig(async (params) => {
+  // Satisfy TypeScript by providing a strict fallback if undefined
+  const locale = params.locale || 'sv';
+
+  if (!locales.includes(locale)) notFound();
+
   return {
-    locale: params.locale,
-    messages: (await import(`../../messages/${params.locale}.json`)).default
+    locale,
+    messages: (await import(`../../messages/${locale}.json`)).default
   };
 });
