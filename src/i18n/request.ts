6e5bc35514bfc5711dc2bdb2262756fba1582323
Ernst-William Hertz <ewilliamhe@gmail.com>
2026-04-07 13:38:58 +0000

    fix: add locale to request config return object
    - RequestConfig type requires both locale and messages
    - Fixes TypeScript error in request.ts

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -2,6 +2,7 @@ import { getRequestConfig } from 'next-intl/server';
 
 export default getRequestConfig(async (params) => {
   return {
+    locale: params.locale,
     messages: (await import(`../../messages/${params.locale}.json`)).default
   };
 });
